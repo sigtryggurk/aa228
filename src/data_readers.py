0b67e7d312750a531ab5dcc6fe34bbd00597418e
Siggi <sigtryggurk@gmail.com>
2018-11-13 18:32:48 -0800

    implement stratified fold

--- src/data_readers.py
+++ src/data_readers.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pandas as pd
 
 from config import Config
+from sklearn.model_selection import StratifiedKFold
 
 def get_wic():
     samples = pd.read_csv(Config.WIC_SAMPLES_FILE, delimiter='\t', header=None, names=["w", "pos", "indices", "ctx1", "ctx2"])
@@ -13,11 +15,18 @@ def get_wic():
 
     labels = pd.read_csv(Config.WIC_LABELS_FILE, header=None, squeeze=True, converters={0 :lambda t: True if t=="T" else False})
     data = samples.assign(is_same = labels)
-
     return data
+
+def get_k_fold_split(data, n_splits=2):
+    assert n_splits >= 2
+
+    skf = StratifiedKFold(n_splits=n_splits, random_state=42, shuffle=False)
+
+    for train_index, test_index in skf.split(np.zeros(len(data)), data.is_same.values):
+        yield data.iloc[train_index], data.iloc[test_index]
 
 if __name__ == "__main__":
     data = get_wic()
-    print(len(data))
-    print(sum(data.is_same))
+    for train, test in get_k_fold_split(data, n_splits=3):
+        print(len(train), len(test))
 
