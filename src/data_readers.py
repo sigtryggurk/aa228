7079545a6bc182a940c8059007d0571e958e0790
Siggi <sigtryggurk@gmail.com>
2018-11-13 18:03:58 -0800

    Parse labels to true/false

--- src/data_readers.py
+++ src/data_readers.py
@@ -11,12 +11,13 @@ def get_wic():
     samples = samples.assign(start1=start1)
     samples = samples.assign(start2=start2)
 
-    labels = pd.read_csv(Config.WIC_LABELS_FILE, header=None)
+    labels = pd.read_csv(Config.WIC_LABELS_FILE, header=None, squeeze=True, converters={0 :lambda t: True if t=="T" else False})
     data = samples.assign(is_same = labels)
 
     return data
 
 if __name__ == "__main__":
     data = get_wic()
-    print(data[:5])
+    print(len(data))
+    print(sum(data.is_same))
 
