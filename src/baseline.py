2708d8e2308a3c929655a87a65f48531ddc91af4
jayadevbhaskaran <jayadev@stanford.edu>
2018-12-06 16:11:16 -0800

    Report AUC also

--- src/baseline.py
+++ src/baseline.py
@@ -6,13 +6,14 @@ from config import Config
 from lightgbm import LGBMClassifier
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 SEED = Config.SEED
 
 def run_baselines(data, clf):
     X_cols = [col_name for col_name in data.columns if "emb_" in col_name]
     acc = [] 
+    auc = []
     for train, dev in dr.get_k_fold_split(data, n_splits=5):
         X_train = train[X_cols]
         X_dev = dev[X_cols]
@@ -21,9 +22,11 @@ def run_baselines(data, clf):
         
         clf.fit(train[X_cols], train['is_same'])
         preds = clf.predict(dev[X_cols])
+        probs = [a[1] for a in clf.predict_proba(dev[X_cols])]
         acc.append(accuracy_score(dev['is_same'], preds))
+        auc.append(roc_auc_score(dev['is_same'], probs))
     
-    return(np.mean(acc))        
+    return(np.mean(acc), np.mean(auc))        
 
 data = dr.get_wic()
 data = dr.add_embeddings(data)
@@ -31,17 +34,18 @@ print("GloVe 300d")
 print("Dummy: ", run_baselines(data, DummyClassifier(random_state=SEED))) 
 print("LogReg: ", run_baselines(data, LogisticRegression(random_state=SEED)))
 print("LGBM: ", run_baselines(data, LGBMClassifier(random_state=SEED)))
-#Dummy:  0.510632163095
-#LogReg:  0.66972953118
-#LGBM:  0.703463118359
 
-print("Multisense FastText")
+print("Multisense FastText (using only .subword)")
 data_multift = dr.get_wic()
 data_multift = dr.add_embeddings(
         data_multift, embed_file=Config.MULTIFT_EMBED_FILE, cased=True)
 print("Dummy: ", run_baselines(data_multift, DummyClassifier(random_state=SEED))) 
 print("LogReg: ", run_baselines(data_multift, LogisticRegression(random_state=SEED)))
 print("LGBM: ", run_baselines(data_multift, LGBMClassifier(random_state=SEED)))
-#Dummy:  0.510632163095
-#LogReg:  0.636257031603
-#LGBM:  0.706614794044
+
+#Model (Accuracy, AUC)
+#Dummy:  (0.51063216309525039, 0.51063216309525039)
+#GloVe 300d LogReg:  (0.66972953118048151, 0.72767358873453902)
+#GloVe 300d LGBM:  (0.70346311835856268, 0.77422512271428801)
+#MultiFT .subword only 300d LogReg:  (0.6362570316029813, 0.68654117742617937)
+#MultiFT .subword only 300d LGBM:  (0.70661479404430561, 0.77924869419679799)
