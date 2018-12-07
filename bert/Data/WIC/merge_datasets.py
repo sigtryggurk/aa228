607a7d4a04a940072f1a9f2288805519645db8d1
Suvadip Paul <titashpaul@gmail.com>
2018-12-07 03:17:00 +0000

    updated the data separating script

--- bert/Data/WIC/merge_datasets.py
+++ bert/Data/WIC/merge_datasets.py
@@ -7,12 +7,12 @@ Created on Thu Dec 06 14:12:30 2018
 
 import pandas as pd
 import numpy as np
+import sklearn.model_selection 
+
 train = pd.read_csv('train.data.txt', delimiter='\t' )
 train_labels = pd.read_csv('train_label.txt')
 
 out = pd.concat([train, train_labels], axis=1)
-msk = np.random.rand(len(out)) < 0.8
-train_final = out[msk]
-dev_final = out[~msk]
+train_final, dev_final = sklearn.model_selection.train_test_split(out, test_size=0.2, random_state=42)
 train_final.to_csv('train.tsv', sep='\t')
 dev_final.to_csv('dev.tsv', sep='\t')
